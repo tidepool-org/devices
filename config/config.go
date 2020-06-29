0aeafc95a88eca69987c9dac8398619af7491db8
Todd Kazakov <todd.kazakov@gmail.com>
2020-06-29 16:49:08 +0300

    Rename env variables to match chart

--- config/config.go
+++ config/config.go
@@ -4,8 +4,8 @@ import "github.com/kelseyhightower/envconfig"
 
 type Config struct {
 	DevicesConfigFilename string `envconfig:"TIDEPOOL_DEVICES_CONFIG_FILENAME" required:"true"`
-	GrpcPort              uint16 `envconfig:"TIDEPOOL_GRPC_PORT" default:"50051" required:"true"`
-	HttpPort              uint16 `envconfig:"TIDEPOOL_HTTP_PORT" default:"8080" required:"true"`
+	GrpcPort              uint16 `envconfig:"TIDEPOOL_GRPC_SERVER_PORT" default:"50051" required:"true"`
+	HttpPort              uint16 `envconfig:"TIDEPOOL_HTTP_SERVER_PORT" default:"8080" required:"true"`
 }
 
 func New() *Config {
